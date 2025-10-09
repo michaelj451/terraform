71d1885a3b54b24b6d1b6e82aeef042693acb8c6
Your Name <Your Name>
2025-10-09 09:19:11 -0500

    test

--- vmware/main.tf
+++ vmware/main.tf
@@ -2,15 +2,12 @@ terraform {
   required_version = ">= 1.6.0"
   required_providers {
     vsphere = {
-      source  = "vmware/vsphere" # updated source
+      source  = "vmware/vsphere"
       version = "~> 2.6"
     }
   }
 }
 
-# -----------------------------
-# Provider (vCenter)
-# -----------------------------
 provider "vsphere" {
   user                 = "mxferguson@mxferguson.com"
   password             = "Xm3nbl00d"
@@ -18,12 +15,7 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
-# -----------------------------
-# Inventory lookups
-# -----------------------------
-data "vsphere_datacenter" "dc" {
-  name = "lab"
-}
+data "vsphere_datacenter" "dc" { name = "lab" }
 
 data "vsphere_host" "esxi" {
   name          = "vsphere1.mxferguson.com"
@@ -45,20 +37,17 @@ data "vsphere_virtual_machine" "tmpl" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# -----------------------------
-# Virtual Machine
-# -----------------------------
 resource "vsphere_virtual_machine" "vm" {
-  name             = "ubuntu22-test-tf-1"                    # REQUIRED
-  resource_pool_id = data.vsphere_host.esxi.resource_pool_id # REQUIRED
-  datastore_id     = data.vsphere_datastore.ds.id            # REQUIRED
+  name             = "ubuntu22-test-tf-1"
+  resource_pool_id = data.vsphere_host.esxi.resource_pool_id
+  datastore_id     = data.vsphere_datastore.ds.id
 
   num_cpus  = 4
   memory    = 8192
   guest_id  = data.vsphere_virtual_machine.tmpl.guest_id
   scsi_type = data.vsphere_virtual_machine.tmpl.scsi_type
 
-  # Don’t block on IP while debugging
+  # while debugging, don't block on IP
   wait_for_guest_ip_timeout   = 0
   wait_for_guest_net_timeout  = 0
   wait_for_guest_net_routable = false
@@ -82,21 +71,16 @@ resource "vsphere_virtual_machine" "vm" {
         domain    = "mxferguson.com"
       }
 
-      # Static IPv4
-      network_interface {
-        ipv4_address = "10.4.5.101" # choose a free IP
-        ipv4_netmask = 24
-      }
+      # ---- DHCP (one block per NIC) ----
+      network_interface {}
 
-      ipv4_gateway    = "10.4.5.1"
+      # optional: keep custom DNS even with DHCP
       dns_server_list = ["10.3.0.151", "10.3.0.152"]
-      dns_suffix_list = ["mxferguson.com"]
+
+      # IMPORTANT: no ipv4_gateway or ipv4_address/ipv4_netmask here
     }
   }
 }
 
-# -----------------------------
-# Outputs
-# -----------------------------
-output "vm_name" { value = vsphere_virtual_machine.vm.name }
+output "vm_name"        { value = vsphere_virtual_machine.vm.name }
 output "vm_power_state" { value = vsphere_virtual_machine.vm.power_state }
